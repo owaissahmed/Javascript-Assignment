3d52825ceab82fd92bb773c4ccee4e0baa41cd12
Owais Ahmed <muhammadowais25122003@gmail.com>
2022-06-25 12:46:56 +0500

    2nd chapter done

--- app.js
+++ app.js
@@ -19,3 +19,61 @@
 // question 5 
 
 console.log ("Hello... I can run JS through my web browser's console")
+
+
+// CHAPTER # 02 
+
+
+// Question 1 
+var username;
+
+// Question 2
+var myName = "Muhammad Owais";
+
+
+
+// Question 3 
+
+    var message = "Hello World";
+    alert(message)
+
+
+
+// Question 4 
+
+    var name = "Owais";
+    var age = 18;
+    var course = "Web and Mobile Development ";
+    var qualification = "Diploma Holder";
+
+    alert("Name: "  +  name +   "\n"  +  "Age: " + age+  "\n"  +  "Course " + course  +  "\n"+ "Qualification: " + qualification);
+
+
+// Question 5 
+
+    alert("PIZZA \n PIZZ \n PIZ \n PI \n P\n");
+
+
+// Question 6
+
+    emailAddress = "muhammadowais25122003@gmail.com"
+    alert("My Email Address is : " + emailAddress);
+
+
+
+// Question 7
+
+    var book = 'A smarter way to learn JavaScript'
+    alert("I'm trying to learn from the book " + book);
+
+
+// Question 8
+
+    document.write("Yah! I can write HTML content through JavaScript)");
+
+
+// Question 9
+
+    var pattern = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+    alert(pattern);
+

852ea9ac6ec521c4c4afb6c0421abc93a888975f
Owais Ahmed <muhammadowais25122003@gmail.com>
2022-06-25 12:36:49 +0500

    1st chapter done

---
+++ app.js
@@ -0,0 +1,21 @@
+// CHAPTER # 01
+
+// question 1 
+    alert("Hello visitors !")
+
+
+// question 2 
+    alert('Error! Please enter a valid password.')
+
+
+// question 3
+    alert('Welcome to JS Land... \n Happy Coding !');
+
+
+// question 4
+    alert('Welcome to JS Land')
+    alert(' Happy Coding !  \n')
+
+// question 5 
+
+console.log ("Hello... I can run JS through my web browser's console")
